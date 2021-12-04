13d4d6efdda4e5006ea00850130de73e46b656c4
Matus Chochlik <chochlik@gmail.com>
2021-12-04 11:22:09 +0100

    [ReflectionTS] Changes in the mirror experimental reflection API

--- _reflexpr/mirror01.cpp
+++ _reflexpr/mirror01.cpp
@@ -2,17 +2,26 @@
 #include <iostream>
 
 enum class weekdays {
-  monday, tuesday, wednesday, thursday, friday, saturday, sunday
+  monday = 1, tuesday, wednesday, thursday, friday, saturday, sunday
 };
 
 template <typename E>
-consteval auto enum_to_string(E e) -> std::string_view {
-  using namespace std::experimental::mirror;
-  return get_name(mirror(E));
+static std::string_view enum_to_string(E e) {
+  return select(
+    unpack(get_enumerators(mirror(weekdays))),
+    [](auto& result, auto mo, auto e) {
+      if (E(get_constant(mo)) == e) {
+        result = get_name(mo);
+      }
+    }, std::string_view{}, e);
 }
 
 int main() {
   using namespace std::experimental::mirror;
+  for_each(unpack(get_enumerators(mirror(weekdays))), [](auto mo){
+      std::cout << get_name(mo) << ": " << get_constant(mo) << std::endl;
+    });
+
   std::cout << enum_to_string(weekdays::monday) << std::endl;
   std::cout << enum_to_string(weekdays::tuesday) << std::endl;
   std::cout << enum_to_string(weekdays::wednesday) << std::endl;
